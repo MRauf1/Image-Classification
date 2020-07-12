46430e8d49c14adc96d14b8f544a4ad3e94fef02
rauf <m_rauf@outlook.com>
2020-07-12 16:15:54 -0500

    Created splits for datasets

--- dataset.py
+++ dataset.py
@@ -9,13 +9,17 @@ from PIL import Image
 class ImageClassificationDataset(Dataset):
     """ Image Classification dataset """
 
-    def __init__(self, images_folder_path, labels_path, image_size = 227):
+    def __init__(self, images_folder_path, labels_path, image_size = 227,
+                    mode = "train", train_val_test_ratio = [0.7, 0.1, 0.2]):
         """
         Args:
             images_folder_path (string): Path to the folder with the images.
             labels_path (string): Path to the labels in .txt format.
             image_size (int): Size of the square image, which will be passed to CNN.
         """
+
+        assert(sum(train_val_test_ratio) == 1.0)
+        assert(mode == "train" or mode == "val" or mode == "test")
 
         self._images_folder_path = images_folder_path
         self._labels_path = labels_path
@@ -32,6 +36,20 @@ class ImageClassificationDataset(Dataset):
                             if isfile(join(self._images_folder_path, file))]
         with open(self._labels_path, "r") as txt_file:
             self._labels = [line.strip() for line in txt_file]
+
+        num_train = int(len(self._image_names) * train_val_test_ratio[0])
+        num_val = int(len(self._image_names) * train_val_test_ratio[1])
+        num_test = int(len(self._image_names) * train_val_test_ratio[2])
+
+        if(mode == "train"):
+            self._image_names = self._image_names[:num_train]
+            self._labels = self._labels[:num_train]
+        elif(mode == "val"):
+            self._image_names = self._image_names[num_train:(num_train + num_val)]
+            self._labels = self._labels[num_train:(num_train + num_val)]
+        elif(mode == "test"):
+            self._image_names = self._image_names[(num_train + num_val):]
+            self._labels = self._labels[(num_train + num_val):]
 
 
     def __len__(self):
--- train.py
+++ train.py
@@ -4,8 +4,8 @@ import torch
 
 
 
-
 if __name__ == "__main__":
 
-    dataset = ImageClassificationDataset("ImageNet/ILSVRC2012_img_val", "ImageNet/ILSVRC2012_validation_ground_truth.txt")
-    dataset.__getitem__(1)
+    dataset = ImageClassificationDataset("ImageNet/ILSVRC2012_img_val", "ImageNet/ILSVRC2012_validation_ground_truth.txt",
+                mode = "train")
+    print(dataset.__len__())
