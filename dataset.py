164e03ec3049e73fd923edbac9503b3d86527cd2
rauf <m_rauf@outlook.com>
2020-07-12 20:39:42 -0500

    Fixed labeling bug

--- dataset.py
+++ dataset.py
@@ -58,10 +58,9 @@ class ImageClassificationDataset(Dataset):
 
     def __getitem__(self, index):
 
-        label_num = self._labels[index]
-        label = torch.tensor([int(label_num)], dtype = torch.int64)
+        label = torch.tensor([int(self._labels[index])], dtype = torch.int64)
 
-        image_name = "ILSVRC2012_val_" + str(label_num.zfill(8)) + ".JPEG"
+        image_name = "ILSVRC2012_val_" + str(index).zfill(8) + ".JPEG"
         image = Image.open(join(self._images_folder_path, image_name)).convert("RGB")
         image = self._image_transforms(image)
 
